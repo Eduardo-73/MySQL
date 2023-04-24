5af78c8929425361126a49420081814b05e4c398
Eduardo <eduardocorreaaranda10@gmail.com>
2023-04-24 23:23:13 +0200

    Examen mañana

--- SimulacroexaTema7.sql
+++ SimulacroexaTema7.sql
@@ -2,12 +2,45 @@ USE `ventapromoscompleta`;
 
 -- 1
 
-select distinct clientes.nomcli, ventas.codventa 
+select distinct clientes.codcli, clientes.nomcli as nombre, sum(precioventa) as Importe 
 from clientes join ventas on
-			clientes.codcli = ventas.codcli
-order by codventa asc;
+			clientes.codcli = ventas.codcli join detalleventa on
+					ventas.codventa = detalleventa.codventa
+group by codcli
+order by Importe desc;
 
 -- 2
 
-select articulos.refart, articulos.desart, concat('(',categorias.descat,')')
-from articulos
+select articulos.desart, concat(nomart, '(',categorias.descat,')') as detalles, date_format(ventas.fecventa,'%M - %Y, %e (%W)') as fecha
+from articulos join categorias on
+						articulos.codcat = categorias.codcat join detalleventa
+							on articulos.refart = detalleventa.refart join ventas
+								on detalleventa.codventa = ventas.codventa
+order by categorias.descat;
+
+-- 3
+
+select avg(precioartpromo), promociones.despromo
+from articulos join catalogospromos
+	on articulos.refart = catalogospromos.refart join promociones
+		on catalogospromos.codpromo = promociones.codpromo
+where year(fecinipromo) = 2012
+group by promociones.codpromo;
+
+-- 4
+
+drop procedure if exists ejer_Exa_4;
+delimiter $$
+create procedure ejer_Exa_4()
+
+begin 
+	select refart, nomart, codcat
+    from articulos
+    where refart not in (select refart
+						from catalogospromos join promociones
+							on catalogospromos.codpromo = promociones.codpromo
+                        where year(fecinipromo) = year(curdate())
+                        );
+end $$
+delimiter ;
+
